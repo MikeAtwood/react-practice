069da17014f8615b2cae9e42d1d0ba17dbf2153d
Deadpool1 <atwood169@yahoo.com>
2023-06-08 14:50:24 -0500

    handleAddTask method

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 class Overview extends Component {
+    state = {
+        tasks: []
+    }
 
+    handleAddTask = (newTask) => {
+        let prevTask = this.state.tasks
+        let updatedTasks = prevTask.push('',newTask)
+        this.setState({tasks:updatedTasks})
+        console.log(this.state.tasks)
 
+    }
 
     render() {
         return (
             <div>
                 <h1>Tasks To Do</h1>
+                <input placeholder='Add new task'></input>
+                <button>Add</button>
             </div>
         )
     }
