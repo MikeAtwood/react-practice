c36280fdd622895fdd4d2b92b233360ad04cf96f
Deadpool1 <atwood169@yahoo.com>
2023-06-09 12:05:13 -0500

    updated to ordered list

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,39 +1,69 @@
 import React, { Component } from 'react'
 
 class AddTask extends Component {
-    state = {
+    constructor(props) {
+        super(props)
+        this.state = {
         newTask: ''
     }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleAddTask = this.handleAddTask.bind(this)
+    }
+
+    handleInputChange = (e) => {
+        this.setState({ newTask: e.target.value })
+    }
+    handleAddTask = () => {
+        const { newTask } = this.state
+        this.props.onAddTask(newTask)
+        this.setState({ newTask: '' })
+    }
+
     render() {
+        const { newTask } = this.state
         return (
             <div>
-                <input placeholder='Add new task'></input>
-                <button>Add</button>
+                <input value={newTask} onChange={this.handleInputChange} placeholder='Add new task'></input>
+                <button onClick={this.handleAddTask}>Add</button>
             </div>
         )
     }
 }
 
 class Overview extends Component {
-    state = {
-        tasks: []
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
+        this.handleAddTask = this.handleAddTask.bind(this)
+    }   
+    handleAddTask(newTask) {
+        this.setState(prevState => ({
+            tasks: [...prevState.tasks, newTask]
+        }))
     }
 
-    handleAddTask = (newTask) => {
-        let prevTask = this.state.tasks
-        let updatedTasks = prevTask.push('',newTask)
-        this.setState({tasks:updatedTasks})
-        console.log(this.state.tasks)
 
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Tasks To Do</h1>
-                <AddTask />
-            </div>
-        )
-    }
+render() {
+    const { tasks } = this.state
+    return (
+        <div>
+            <h1>Tasks To Do</h1>
+            <AddTask onAddTask={this.handleAddTask} />
+            <ol>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ol>
+        </div>
+    )
+}
 }
 export default Overview
+
+// handleAddTask = (newTask) => {
+    //     let prevTask = this.state.tasks
+    //     let updatedTasks = prevTask.push('',newTask)
+    //     this.setState({tasks:updatedTasks})
+    //     console.log(this.state.tasks)
